62d188c5a7333a35724f1f90ed45de8b50924b3c
ka-yanaoka <119591084+ka-yanaoka@users.noreply.github.com>
2024-07-02 11:43:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ echo "1";
 $filename = '/mnt/filestream.txt';
  
 //ファイルがすでに存在する場合には処理を行わない
-if(!file_exists($filename)){
-    touch($filename);
-}
+//if(!file_exists($filename)){
+//    touch($filename);
+//}
 
 echo "2";
